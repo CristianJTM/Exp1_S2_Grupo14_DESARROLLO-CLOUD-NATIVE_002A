4bf439f359d36ac51161ebb79d3a1643a0c568af
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-05-25 06:07:27 -0400

    add duration and cost fields to CursoDTO, and create CursoResumenDTO and InscripcionResumenDTO classes

--- src/main/java/com/duoc/CloudLearningPlatform/dto/CursoDTO.java
+++ src/main/java/com/duoc/CloudLearningPlatform/dto/CursoDTO.java
@@ -4,13 +4,17 @@ public class CursoDTO {
     private String nombre;
     private String descripcion;
     private Long profesorId;
+    private Integer duracion;
+    private Double costo;
 
     public CursoDTO(){}
 
-    public CursoDTO(String nombre, String descripcion, Long profesorId) {
+    public CursoDTO(String nombre, String descripcion, Long profesorId,  Integer duracion, Double costo) {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.profesorId = profesorId;
+        this.duracion = duracion;
+        this.costo = costo;
     }
 
     public String getNombre() {
@@ -34,5 +38,21 @@ public class CursoDTO {
 
     public void setProfesorId(Long profesorId) {
         this.profesorId = profesorId;
+    }
+
+    public Integer getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Integer duracion) {
+        this.duracion = duracion;
+    }
+
+    public Double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(Double costo) {
+        this.costo = costo;
     }
 }
