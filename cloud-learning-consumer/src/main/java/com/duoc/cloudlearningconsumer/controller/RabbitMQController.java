d1940b882d3293f45e2d9a2f8ce46cb07a69d382
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-07-06 22:48:05 -0400

    Add endpoint to list ResumenInscripcion records in RabbitMQController

--- cloud-learning-consumer/src/main/java/com/duoc/cloudlearningconsumer/controller/RabbitMQController.java
+++ cloud-learning-consumer/src/main/java/com/duoc/cloudlearningconsumer/controller/RabbitMQController.java
@@ -1,9 +1,13 @@
 package com.duoc.cloudlearningconsumer.controller;
 
+import com.duoc.cloudlearningconsumer.model.ResumenInscripcion;
+import com.duoc.cloudlearningconsumer.repository.ResumenInscripcionRepository;
 import com.duoc.cloudlearningconsumer.service.RabbitMQConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/consumer")
@@ -12,11 +16,19 @@ public class RabbitMQController {
     @Autowired
     private RabbitMQConsumer rabbitMQConsumer;
 
+    @Autowired
+    private ResumenInscripcionRepository resumenInscripcionRepository;
+
     @PostMapping("/procesar")
     public ResponseEntity<String> procesarMensaje() {
 
         rabbitMQConsumer.consumirMensaje();
 
         return ResponseEntity.ok("Resumen procesado correctamente.");
+    }
+
+    @GetMapping("/resumenes")
+    public List<ResumenInscripcion> listar() {
+        return resumenInscripcionRepository.findAll();
     }
 }
